1fd340b67dd7b8cb02559ac9e73812b0b09785bc
Zeqi Zhu <zeqi.zhu@polynom.io>
2021-07-26 06:59:47 +0800

    update yolo-s

--- models/yolo-s-mish.py
+++ models/yolo-s-mish.py
@@ -25,6 +25,14 @@ from models.detector import Detect
 # from module import Concat
 # from detector import Detect
 
+'''
+Conv2D, Conv, Bottleneck, SPP, DWConv, Focus, BottleneckCSP, BottleneckCSP2, SPPCSP, VoVCSP
+filters = math.ceil(c2 * width_multiple / 8) * 8 if c2 != output_dims else c2
+
+[BottleneckCSP, BottleneckCSP2, SPPCSP, VoVCSP]
+number = max(round(number * depth_multiple), 1) if number > 1 else number
+'''
+
 
 class YoloL(object):
     def __init__(self, use_bias, add_bn, add_mish, yaml_dir):
@@ -51,6 +59,13 @@ class YoloL(object):
         x = tf.keras.Input([img_size, img_size, 3])
         output = self.forward(x)
         return tf.keras.Model(inputs=x, outputs=output, name=name)
+    
+    def get_filter_num(self, n_filters):
+        return math.ceil(n_filters * self.width_multiple / 8) * 8
+    
+    def get_layer_num(self, layer_number):
+        return max(round(layer_number * self.depth_multiple), 1) 
+
 
     def build_backbone(self, x):
         backbone_logs = {}
@@ -58,43 +73,48 @@ class YoloL(object):
         x = Focus(x, 64, 3, 1, self.use_bias, self.add_bn, self.add_mish)
 
         # 1-P2/4
-        x = CBM(x, 128, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'P2_1')
+        n_filters = self.get_filter_num(128)
+        x = CBM(x, n_filters, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'P2_1')
 
         # 2-P2/4
-        layer_number = 3
-        layer_number = max(round(layer_number * self.depth_multiple), 1) 
-        x = BottleneckCSP(x, 128, self.use_bias, self.add_bn, self.add_mish, 
+        n_filters = self.get_filter_num(128)
+        layer_number = self.get_layer_num(3)
+        x = BottleneckCSP(x, n_filters, self.use_bias, self.add_bn, self.add_mish, 
                 n_layer=layer_number, shortcut=True, expansion=0.5, block_id='P2_2')
 
         # 3-P3/8
-        x = CBM(x, 256, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'P3_1')
+        n_filters = self.get_filter_num(256)
+        x = CBM(x, n_filters, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'P3_1')
 
         # 4-P3/8
-        layer_number = 9
-        layer_number = max(round(layer_number * self.depth_multiple), 1) 
-        x = BottleneckCSP(x, 256, self.use_bias, self.add_bn, self.add_mish, 
+        n_filters = self.get_filter_num(256)
+        layer_number = self.get_layer_num(9)
+        x = BottleneckCSP(x, n_filters, self.use_bias, self.add_bn, self.add_mish, 
                 n_layer=layer_number, shortcut=True, expansion=0.5, block_id='P3_2')
         backbone_logs['P3'] = x
 
         # 5-P4/16
-        x = CBM(x, 512, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'P4_1')
+        n_filters = self.get_filter_num(512)
+        x = CBM(x, n_filters, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'P4_1')
         
         # 6-P4/16
-        layer_number = 9
-        layer_number = max(round(layer_number * self.depth_multiple), 1) 
-        x = BottleneckCSP(x, 512, self.use_bias, self.add_bn, self.add_mish, 
+        n_filters = self.get_filter_num(512)
+        layer_number = self.get_layer_num(9)
+        x = BottleneckCSP(x, n_filters, self.use_bias, self.add_bn, self.add_mish, 
                     n_layer=layer_number, shortcut=True, expansion=0.5, block_id='P4_2')
         backbone_logs['P4'] = x
 
         # 7-P5/32
-        x = CBM(x, 1024, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'P5_1')
+        n_filters = self.get_filter_num(1024)
+        x = CBM(x, n_filters, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'P5_1')
 
         # 8-P5/32
-        x = SPP(x, 1024, self.use_bias, self.add_bn, self.add_mish, kernels=(5, 9, 13), block_id='P5_SPP')
+        n_filters = self.get_filter_num(1024)
+        x = SPP(x, n_filters, self.use_bias, self.add_bn, self.add_mish, kernels=(5, 9, 13), block_id='P5_SPP')
 
         # 9-P5/32
-        layer_number = 3
-        layer_number = max(round(layer_number * self.depth_multiple), 1) 
+        n_filters = self.get_filter_num(1024)
+        layer_number = self.get_layer_num(3)
         x = BottleneckCSP(x, 1024, self.use_bias, self.add_bn, self.add_mish, 
                     n_layer=layer_number, shortcut=False, expansion=0.5, block_id='P5_2')
         backbone_logs['backbone_out'] = x
@@ -105,7 +125,8 @@ class YoloL(object):
 
         # 10-P5/32
         x = backbone_logs['backbone_out']
-        x = CBM(x, 512, 1, 1, 1, self.use_bias, self.add_bn, self.add_mish, 'head_10')
+        n_filters = self.get_filter_num(512)
+        x = CBM(x, n_filters, 1, 1, 1, self.use_bias, self.add_bn, self.add_mish, 'head_10')
         head_logs['head_P5'] = x
 
         # 11-P4/16
@@ -115,13 +136,14 @@ class YoloL(object):
         x = Concatenate(axis=-1)([x, backbone_logs['P4']])
 
         # 13-P4/16
-        layer_number = 3
-        layer_number = max(round(layer_number * self.depth_multiple), 1) 
-        x = BottleneckCSP(x, 512, self.use_bias, self.add_bn, self.add_mish, 
+        n_filters = self.get_filter_num(512)
+        layer_number = self.get_layer_num(3)
+        x = BottleneckCSP(x, n_filters, self.use_bias, self.add_bn, self.add_mish, 
                     n_layer=layer_number, shortcut=False, expansion=0.5, block_id='head_13')
 
         # 14-P4/16
-        x = CBM(x, 256, 1, 1, 1, self.use_bias, self.add_bn, self.add_mish, 'head_14')
+        n_filters = self.get_filter_num(256)
+        x = CBM(x, n_filters, 1, 1, 1, self.use_bias, self.add_bn, self.add_mish, 'head_14')
         head_logs['head_P4'] = x
 
         # 15-P3/8
@@ -131,35 +153,37 @@ class YoloL(object):
         x = Concatenate(axis=-1)([x, backbone_logs['P3']])
 
         # 17-P3/8 -small
-        layer_number = 3
-        layer_number = max(round(layer_number * self.depth_multiple), 1) 
-        x = BottleneckCSP(x, 256, self.use_bias, self.add_bn, self.add_mish, 
+        n_filters = self.get_filter_num(256)
+        layer_number = self.get_layer_num(3)
+        x = BottleneckCSP(x, n_filters, self.use_bias, self.add_bn, self.add_mish, 
                     n_layer=layer_number, shortcut=False, expansion=0.5, block_id='head_17')
         head_logs['detect_P3'] = x
 
         # 18-P4/16
-        x = CBM(x, 256, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'head_18')
+        n_filters = self.get_filter_num(256)
+        x = CBM(x, n_filters, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'head_18')
 
         # 19-P4/16 cat head P4
         x = Concatenate(axis=-1)([x, head_logs['head_P4']])
 
         # 20 (P4/16-medium)
-        layer_number = 3
-        layer_number = max(round(layer_number * self.depth_multiple), 1) 
-        x = BottleneckCSP(x, 512, self.use_bias, self.add_bn, self.add_mish, 
+        n_filters = self.get_filter_num(512)
+        layer_number = self.get_layer_num(3)
+        x = BottleneckCSP(x, n_filters, self.use_bias, self.add_bn, self.add_mish, 
                     n_layer=layer_number, shortcut=False, expansion=0.5, block_id='head_20')
         head_logs['detect_P4'] = x
 
         # 21-P5/8
-        x = CBM(x, 512, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'head_21')
+        n_filters = self.get_filter_num(512)
+        x = CBM(x, n_filters, 3, 2, 1, self.use_bias, self.add_bn, self.add_mish, 'head_21')
 
         # 22-P5/8 cat head P5
         x = Concatenate(axis=-1)([x, head_logs['head_P5']])
 
         # 23 (P5/32-large)
-        layer_number = 3
-        layer_number = max(round(layer_number * self.depth_multiple), 1) 
-        x = BottleneckCSP(x, 1024, self.use_bias, self.add_bn, self.add_mish, 
+        n_filters = self.get_filter_num(1024)
+        layer_number = self.get_layer_num(3)
+        x = BottleneckCSP(x, n_filters, self.use_bias, self.add_bn, self.add_mish, 
                     n_layer=layer_number, shortcut=False, expansion=0.5, block_id='head_23')
         head_logs['detect_P5'] = x
         return head_logs
@@ -172,9 +196,9 @@ class YoloL(object):
 
 
 if __name__ == '__main__':
-    yaml_path = '../models/configs/yolo-l-mish.yaml'
+    yaml_path = '../models/configs/yolo-s-mish.yaml'
     yolo = YoloL(use_bias=False, add_bn=True, add_mish=True, yaml_dir=yaml_path)
-    model = yolo(img_size=640, name='yolo_l')
+    model = yolo(img_size=640, name='yolo_s')
     model.summary()
 
     for layer in model.layers:
@@ -183,4 +207,4 @@ if __name__ == '__main__':
     net = tf.keras.Model(inputs=model.input, outputs=model.get_layer('mish_111').output)
     net.summary()
 
-    net.save('yolo-l-mish.h5', save_format='tf')
+    net.save('yolo-s-mish.h5', save_format='tf')
